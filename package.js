a448422320c11937062858ac901e9365189be164
jotaroxyz <bartek16100priv@gmail.com>
2026-07-07 18:08:13 +0200

    refactor: use electron-builder config inside package.js instead .json. fix: artifact names.

---
+++ package.js
@@ -0,0 +1,48 @@
+/**
+ * @type {import('electron-builder').Configuration}
+ */
+export default {
+  appId: "com.jotaroxyz.imageResizer",
+  productName: "ImageResizer",
+  directories: {
+    output: "package"
+  },
+  files: [
+    "package.json",
+    {
+      from: "dist/electron",
+      to: "dist/electron"
+    },
+    {
+      from: "dist/react",
+      to: "dist/react"
+    }
+  ],
+  icon: "./icon.png",
+  extraResources: ["dist/electron/preload.cjs"],
+  publish: {
+    provider: "github",
+    owner: "jotaroxyz",
+    repo: "imageResizer",
+    publishAutoUpdate: false
+  },
+  mac: {
+    target: "dmg",
+    artifactName: "${productName}-${os}-${arch}.${ext}"
+  },
+  linux: {
+    target: "AppImage",
+    category: "Utility",
+    artifactName: "${productName}-${os}-${arch}.${ext}"
+  },
+  win: {
+    target: ["portable", "msi"],
+    artifactName: "${productName}-${os}-${arch}.${ext}"
+  },
+  msi: {
+    artifactName: "${productName}-${os}-${arch}_installer.${ext}"
+  },
+  portable: {
+    artifactName: "${productName}-${os}-${arch}_portable.${ext}"
+  }
+};
